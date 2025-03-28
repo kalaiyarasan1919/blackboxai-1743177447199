56a3a1173f5d322bd936adbe8c19fa35eff87c0d
kalaiyarasan <149139606+kalaiyarasan1919@users.noreply.github.com>
2025-03-28 21:28:29 +0530

    Add frontend/src/pages/EditProject.js 1743177508525

---
+++ frontend/src/pages/EditProject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectForm from '../components/ProjectForm';
+
+const EditProject = () => {
+  return (
+    <div>
+      <ProjectForm editMode={true} />
+    </div>
+  );
+};
+
+export default EditProject;
