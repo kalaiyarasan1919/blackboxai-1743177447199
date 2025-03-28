cd018bb82cdd87bb480d4d55339b430145d0e642
kalaiyarasan <149139606+kalaiyarasan1919@users.noreply.github.com>
2025-03-28 21:27:47 +0530

    Add backend/config/db.js 1743177466881

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
